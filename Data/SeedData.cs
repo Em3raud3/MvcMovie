edd8f0dbf26744cfd5ee9d8bc7418c2ac907f4ce
Em3raud3 <animekamisama@gmail.com>
2021-10-25 01:28:35 -0400

    added a new field

--- Data/SeedData.cs
+++ Data/SeedData.cs
@@ -26,7 +26,7 @@ namespace MvcMovie.Models
                         Title = "When Harry Met Sally",
                         ReleaseDate = DateTime.Parse("1989-2-12"),
                         Genre = "Romantic Comedy",
-                        Price = 7.99M
+                        Price = 7.99M,
                         Rating = "G"
                     },
 
@@ -35,7 +35,7 @@ namespace MvcMovie.Models
                         Title = "Ghostbusters ",
                         ReleaseDate = DateTime.Parse("1984-3-13"),
                         Genre = "Comedy",
-                        Price = 8.99M
+                        Price = 8.99M,
                         Rating = "PG"
                     },
 
@@ -44,7 +44,7 @@ namespace MvcMovie.Models
                         Title = "Ghostbusters 2",
                         ReleaseDate = DateTime.Parse("1986-2-23"),
                         Genre = "Comedy",
-                        Price = 9.99M
+                        Price = 9.99M,
                         Rating = "PG"
                     },
 
@@ -53,7 +53,7 @@ namespace MvcMovie.Models
                         Title = "Rio Bravo",
                         ReleaseDate = DateTime.Parse("1959-4-15"),
                         Genre = "Western",
-                        Price = 3.99M
+                        Price = 3.99M,
                         Rating = "R"
                     }
                 );
