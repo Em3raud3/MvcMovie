f38a942e3578dcd851787e9bc9f02c07594ef8cc
Em3raud3 <animekamisama@gmail.com>
2021-10-25 01:21:34 -0400

    before updating database

--- Data/SeedData.cs
+++ Data/SeedData.cs
@@ -27,6 +27,7 @@ namespace MvcMovie.Models
                         ReleaseDate = DateTime.Parse("1989-2-12"),
                         Genre = "Romantic Comedy",
                         Price = 7.99M
+                        Rating = "G"
                     },
 
                     new Movie
@@ -35,6 +36,7 @@ namespace MvcMovie.Models
                         ReleaseDate = DateTime.Parse("1984-3-13"),
                         Genre = "Comedy",
                         Price = 8.99M
+                        Rating = "PG"
                     },
 
                     new Movie
@@ -43,6 +45,7 @@ namespace MvcMovie.Models
                         ReleaseDate = DateTime.Parse("1986-2-23"),
                         Genre = "Comedy",
                         Price = 9.99M
+                        Rating = "PG"
                     },
 
                     new Movie
@@ -51,6 +54,7 @@ namespace MvcMovie.Models
                         ReleaseDate = DateTime.Parse("1959-4-15"),
                         Genre = "Western",
                         Price = 3.99M
+                        Rating = "R"
                     }
                 );
                 context.SaveChanges();
